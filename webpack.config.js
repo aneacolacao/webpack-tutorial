fb214cacd5e91d2997173a56580714967ee8b1f1
Andrea Cadena <andreacadena@Mac-mini-de-Andrea.local>
2018-08-01 16:02:36 -0500

    NPM Watch. installing Babel and SCSS Loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,22 @@ let config = {
     entry: './index.js',
     output: {
         filename: 'output.js'
+    },
+    mode: 'none',
+    module: {
+        rules: [
+            {
+                test: /\.js$/, // files ending with .js
+                exclude: /node_modules/, //exclude the node_modules directory
+                loader: "babel-loader" //user this (babel-core)loader
+            },
+            {
+                test: /\.scss$/, // files ending with .scss
+                loader: ['style-loader','css-loader', 'sass-loader'] // user these loaders
+            }
+        ]
     }
+
 }
 
 module.exports = config;
